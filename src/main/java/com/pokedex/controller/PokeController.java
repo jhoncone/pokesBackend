2c60f24e4edb5f2eacf38a9fd43bcc5008485407
htm <destokp@gmail.com>
2021-11-07 21:03:33 -0600

    add via upload

--- src/main/java/com/pokedex/controller/PokeController.java
+++ src/main/java/com/pokedex/controller/PokeController.java
@@ -11,88 +11,72 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 @RequestMapping("/api/poke")
 public class PokeController {
-	
+
 	@Autowired
 	private RestTemplate clienteRest;
 
-
-    @RequestMapping("/all")
-    public List<Pokemon> getPokes(){
+	@RequestMapping("/all")
+	public List<Pokemon> getPokes() {
 		List<Pokemon> pokes = new ArrayList<>();
-		Result result = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20",
-				  Result.class);
-		int i=1;
+		Result result = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20", Result.class);
+		int i = 1;
 		for (Pokemon poke : result.getLisPokemon()) {
-		poke = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/"+i+"/", Pokemon.class); 
-		pokes.add(poke);
-		i++;
+			poke = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/" + i + "/", Pokemon.class);
+			pokes.add(poke);
+			i++;
 		}
 		return pokes;
-    }
+	}
 
-    @RequestMapping("/{id}/")
-    public ArrayList<String> getInfo(@PathVariable("id") int id){
-        RestTemplate clienteRest = new RestTemplate();
+	@RequestMapping("{id}/info")
+	public PokemonJson getPokemon(@PathVariable("id") String id) {
+		RestTemplate clienteRest = new RestTemplate();
+		ArrayList<String> data = new ArrayList<String>();
+		Pokemon p;
+		p = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/" + id, Pokemon.class);
+		PokemonJson pokemon = new PokemonJson();
+		pokemon.setId(p.getId());
+		pokemon.setName(p.getName());
+		pokemon.setWeight(p.getWeight());
+		pokemon.setImage(p.getSprites().getFront_default());
 
-        ArrayList<String> data = new ArrayList<String>();
-        Chain c = clienteRest.getForObject("https://pokeapi.co/api/v2/evolution-chain/" + id, Chain.class);
-        assert c != null;
-        data.add(c.getChain().getSpecies().getName());
+		for (int i = 0; i < p.getAbilities().size(); i += 1) {
+			pokemon.addAbility(p.getAbilities().get(i).getAbility().getName());
+		}
+		for (int i = 0; i < p.getTypes().size(); i += 1) {
+			pokemon.addType(p.getTypes().get(i).getType().getName());
+		}
+		return pokemon;
+	}
 
-        Pokemon p = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/" + c.getChain().getSpecies().getName(), Pokemon.class);
-        assert p != null;
-        data.add(String.valueOf(p.getId()));
+	@RequestMapping("/{id}/evoluciones")
+	public EvolutionJson getEvolutions(@PathVariable("id") int id) {
+		RestTemplate clienteRest = new RestTemplate();
+		if ((id) > ((id) / 3)) {
+			id = (int) Math.ceil((id) / (3.0));
+		}
 
-        return data;
-    }
-    @RequestMapping("{id}/info")
-    public PokemonJson getPokemon(@PathVariable("id") String id){
-        RestTemplate clienteRest = new RestTemplate();
+		Chain p = clienteRest.getForObject("https://pokeapi.co/api/v2/evolution-chain/" + id, Chain.class);
+		EvolutionJson evoluciones = new EvolutionJson();
+		assert p != null;
 
-        ArrayList<String> data = new ArrayList<String>();
-        Pokemon p;
-        if(id.matches("-?\\d+")) {
-            Chain c = clienteRest.getForObject("https://pokeapi.co/api/v2/evolution-chain/" + id, Chain.class);
-            p = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/" + c.getChain().getSpecies().getName(), Pokemon.class);
-            assert p != null;
-        }
-        else
-            p = clienteRest.getForObject("https://pokeapi.co/api/v2/pokemon/" + id, Pokemon.class);
-        PokemonJson pokemon = new PokemonJson();
-        pokemon.setId(p.getId());
-        pokemon.setName(p.getName());
-        pokemon.setWeight(p.getWeight());
-        pokemon.setImage(p.getSprites().getFront_default());
+		Evolution evo = p.getChain();
 
-        for(int i=0;i<p.getAbilities().size();i+=1){
-            pokemon.addAbility(p.getAbilities().get(i).getAbility().getName());
-        }
-        for(int i=0;i<p.getTypes().size();i+=1){
-            pokemon.addType(p.getTypes().get(i).getType().getName());
-        }
-        return pokemon;
-    }
+		while (evo != null) {
 
-    @RequestMapping("/{id}/evoluciones")
-    public EvolutionJson getEvolutions(@PathVariable("id") int id){
-        RestTemplate clienteRest = new RestTemplate();
-        Chain p = clienteRest.getForObject("https://pokeapi.co/api/v2/evolution-chain/" + id, Chain.class);
-        EvolutionJson evoluciones = new EvolutionJson();
-        assert p != null;
-        Evolution evo = p.getChain();
-        while(evo!=null){
-            evoluciones.addPokemon(evo.getSpecies().getName());
-            if(!evo.getEvolves_to().isEmpty())
-                evo = evo.getEvolves_to().get(0);
-            else
-                evo = null;
-        }
-        return evoluciones;
-    }
+			evoluciones.addPokemon(evo.getSpecies().getName());
 
+			if (!evo.getEvolves_to().isEmpty())
+				evo = evo.getEvolves_to().get(0);
+			else
+				evo = null;
 
+		}
+
+		return evoluciones;
+	}
 
 }
